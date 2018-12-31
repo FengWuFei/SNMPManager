88754c0ba91154ff346c6e39aeb12c2293a206a2
YiLiu <939488764@qq.com>
2018-12-31 10:34:48 +0800

    fix

--- Sources/SNMPManager/SNMPMessages/SNMPMessage.swift
+++ Sources/SNMPManager/SNMPMessages/SNMPMessage.swift
@@ -21,6 +21,14 @@ public struct SNMPMessage {
     public var version: SNMPVersion
     public var community: String
     public var pdu: PDU
+    public var valueBinds: [(key: String, value: BerTagedObject)] {
+        switch pdu {
+        case .basic(let pdu):
+            return pdu.valueBinds.dic
+        case .v1Trap(let pdu):
+            return pdu.valueBinds.dic
+        }
+    }
     
     public init(version: SNMPVersion, community: String, pdu: PDU) {
         self.version = version
